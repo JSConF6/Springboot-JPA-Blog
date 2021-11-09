42bc18941df6611d28f47b770ff0ce7cf2deb474
JSConF <arare30030@naver.com>
2021-11-10 00:14:47 +0900

    Dummy Data 요청 응답 테스트 완료

--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -4,12 +4,15 @@ import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,13 +25,14 @@ import lombok.NoArgsConstructor;
 @Data // Getter Setter 합친것
 //ORM -> Java(다른언어) Object -> 테이블로 매핑해주는 기술
 @Entity // User 클래스가 MySQL에 테이블이 생성이 된다.
+// @DynamicInsert // insert 시에 null인 필드를 제외시켜준다.
 public class User {
 	
 	@Id // Primary key
 	@GeneratedValue(strategy = GenerationType.IDENTITY)// 프로젝트에서 연결된 DB의 넘버링 전략을 따라간다. (오라클 : 시퀀스, MySQL : auto_increment)
 	private int id; // 오라클 : 시퀀스, MySQL :  auto_increment
 	
-	@Column(nullable = false, length = 30)
+	@Column(nullable = false, length = 30, unique = true)
 	private String username; // 아이디
 	
 	@Column(nullable = false, length = 100) // 123456 => 해쉬(비밀번호 암호화)
@@ -37,8 +41,10 @@ public class User {
 	@Column(nullable = false, length = 50)
 	private String email;
 	
-	@ColumnDefault("'user'")
-	private String role; // Enum을 쓰는게 좋다. // admin, user, manager
+	//	@ColumnDefault("user")
+	// DB는 RoleType이라는 게 없다.
+	@Enumerated(EnumType.STRING)
+	private RoleType role; // Enum을 쓰는게 좋다. // ADMIN, USER
 	
 	@CreationTimestamp // 시간이 자동 입력
 	private Timestamp createDate; // 가입한 날짜
